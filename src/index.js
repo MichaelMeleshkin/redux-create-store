4b770164c28839680aeb8067207e6f0e52e64ad5
Michael Meleshkin <mmeleshkin@logika.com>
2019-02-14 16:22:28 +0200

    Custom redux createStore

--- src/index.js
+++ src/index.js
@@ -1,12 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Store {
+    constructor(reducer) {
+        this.reducer = reducer;
+        this._getState();
+        this.subscribers = [];
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    _getState(action = {}) {
+        return this.state = this.reducer(this.state, action);
+    }
+  
+    getState() {
+        return this._getState();
+    }
+
+    dispatch(obj) {
+        let { type } = obj;
+        this._getState({ type })
+        this.subscribers.forEach((callback) => {
+            if (callback) {
+                callback();
+            }
+        });
+    }
+
+    subscribe(fn) {
+        this.subscribers.push(fn);
+        return () => {
+            this.subscribers.length = 0;
+        }
+    }
+}
+
+let reducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'REMOVE':
+            return state - 1;
+        case 'ADD':
+            return state + 1;
+        default:
+            return state
+    }
+}
+
+let store = new Store(reducer);
+window.s = store;
+
+function App() {
+    return (
+        <div>
+            <div id="counter"></div>
+            <button id="add">Add</button>
+            <button id="remove">Remove</button>
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'), () => {
+    document.getElementById('add').addEventListener('click', () => { store.dispatch({ type: 'ADD' }); });
+    document.getElementById('remove').addEventListener('click', () => { store.dispatch({ type: 'REMOVE' }); });
+
+    function updateCounter() { document.getElementById('counter').textContent = store.getState(); };
+    updateCounter();
+    store.subscribe(() => { updateCounter(); })
+});
